0db35755a7c5deefc30cfadfba839c9daeb37008
SeoJiWon <sjiwon4491@gmail.com>
2023-11-30 21:32:50 +0900

    init.sql 활성화

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -9,8 +9,3 @@ CREATE TABLE IF NOT EXISTS contacts
     PRIMARY KEY (id)
 ) ENGINE = INNODB
   DEFAULT CHARSET = utf8mb4;
-
-INSERT INTO contacts(name, age, phone, created_at)
-VALUES ('가나다', 20, '010-1111-2222', '2023-11-27 13:48:23'),
-       ('라라라', 25, '010-2222-3333', '2023-11-27 15:30:38'),
-       ('홍길동', 30, '010-1111-4444', '2023-11-27 18:30:38')
